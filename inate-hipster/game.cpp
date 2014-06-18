f1fe5359a23739d8b22988aeb84f60c41a58b28a
randomcrispy <drewatkinson5@gmail.com>
2014-06-18 18:04:28 -0400

    said hi to phillip

--- inate-hipster/game.cpp
+++ inate-hipster/game.cpp
@@ -27,6 +27,9 @@ const int SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 720;
 const int SCREEN_FPS = 120;
 const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
+/*
+HIII PHILLIUPPPPPPPPPPP
+*/
 
 //Prototypes
 bool init();
