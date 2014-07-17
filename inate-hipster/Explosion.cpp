b79aec0f84aeb1696e70878aedc5126d9181d11c
randomcrispy <drewatkinson5@gmail.com>
2014-07-17 17:39:46 -0400

    farts

---
+++ inate-hipster/Explosion.cpp
@@ -0,0 +1,63 @@
+#include "Explosion.h"
+
+
+Explosion::Explosion(int numRays, int blastPower) : numRays(numRays), blastPower(blastPower)
+{
+	if (numRays > MAX_BLAST_RAYS)
+		numRays = MAX_BLAST_RAYS;
+}
+
+Explosion::~Explosion()
+{
+	clearParticles();
+}
+
+
+void Explosion::explodeParticle(b2Vec2* loc)
+{
+	clearParticles();
+
+	if (loc) 
+	{
+		for (int i = 0; i < numRays; i++) 
+		{
+			float angle = (i / (float)numRays) * 360 * 0.0174532925199432957f;
+			b2Vec2 rayDir(sinf(angle), cosf(angle));
+
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.fixedRotation = true;
+			bd.bullet = true;
+			bd.linearDamping = 10;
+			bd.gravityScale = 0;
+			bd.position = *loc;
+			bd.linearVelocity = 0.125f * blastPower * rayDir;
+			b2Body* body = worldptr->CreateBody(&bd);
+
+			b2CircleShape circleShape;
+			circleShape.m_radius = 0.05;
+
+			b2FixtureDef fd;
+			fd.shape = &circleShape;
+			fd.density = 60 / (float)numRays;
+			fd.friction = 0;
+			fd.restitution = 0.99f;
+			fd.filter.groupIndex = -1;
+			body->CreateFixture(&fd);
+
+			blastParticleBodies[i] = body;
+		}
+	}
+}
+
+void Explosion::clearParticles()
+{
+	//clear old particles
+	for (int i = 0; i < MAX_BLAST_RAYS; i++) {
+		if (blastParticleBodies[i]) {
+			worldptr->DestroyBody(blastParticleBodies[i]);
+			blastParticleBodies[i] = NULL;
+		}
+	}
+}
+
---
+++ inate-hipster/Explosion.h
@@ -0,0 +1,28 @@
+/*
+	Parts adapted from Chris Campbell -- http://www.iforce2d.net/b2dtut/explosions
+*/
+
+#pragma once
+
+#include <Box2D\Box2D.h>
+#include "worldConst.h"
+
+const int MAX_BLAST_RAYS = 120;
+
+class Explosion
+{
+public:
+	Explosion(int numRays, int blastPower);
+	~Explosion();
+	void blast();
+	void Explosion::explodeParticle(b2Vec2* loc);
+	void Explosion::clearParticles();
+	
+private:
+	
+	b2Body*	blastParticleBodies[MAX_BLAST_RAYS];
+	int numRays;
+	int blastPower;
+
+};
+
